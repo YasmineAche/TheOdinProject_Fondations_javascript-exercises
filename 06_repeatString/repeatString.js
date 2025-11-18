5268425cf99bb2fee107eda05f78e01e1d80e303
YasmineAche <128965544+YasmineAche@users.noreply.github.com>
2025-11-18 12:48:34 +0100

    Adding solution file

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return "0 is not a valid input";
+    let newStr = "";
+    for (let i = 0; i < num; i++) {
+        newStr += string;
+    }
+    return newStr;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
