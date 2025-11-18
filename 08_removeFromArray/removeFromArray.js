30eb47c340213b4235996cb047bf6005dc061d31
YasmineAche <128965544+YasmineAche@users.noreply.github.com>
2025-11-18 14:39:25 +0100

    Adding solution file

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+    let args = Array.from(arguments);
+    if (args.length <= 1) {
+        console.log("Not enough arguments");
+        return;
+    }
+    for(let num of args.slice(1)) {
+        if (args[0].includes(num)) {
+          args[0].splice(args[0].indexOf(num), 1);
+        }
+    }
+    return args[0];
 };
-
+console.log(removeFromArray([1, 2, 3, 4],3));
 // Do not edit below this line
 module.exports = removeFromArray;
