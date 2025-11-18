f571cd1a9cc92bb79ff4138ccf9c223cdf903b86
YasmineAche <128965544+YasmineAche@users.noreply.github.com>
2025-11-18 15:08:50 +0100

    Adding solution file

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  let sum = 0;
+  a = parseInt(a);
+  b = parseInt(b);
+  if (isNaN(a) || isNaN(b)) return "ERROR";
+  else {
+    if (a > b) {
+      a, (b = b), a;
+    }
+    for (let i = a; i <= b; i++) {
+      sum += i;
+    }
+    return sum;
+  }
 };
-
 // Do not edit below this line
 module.exports = sumAll;
